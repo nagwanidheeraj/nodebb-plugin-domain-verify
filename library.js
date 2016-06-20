a198ddcdf708fc1fd3c28097eb11b2e81f9a4ce0
DeeJay <dheeraj@nagwani.in>
2016-06-20 16:51:23 +0530

    Modified quickstart to add settings and check email domain

--- library.js
+++ library.js
@@ -1,8 +1,12 @@
 "use strict";
 
-var controllers = require('./lib/controllers'),
+var winston = module.parent.require('winston'),
+    Meta = module.parent.require('./meta'),
+    controllers = require('./lib/controllers'),
 
-	plugin = {};
+    plugin = {};
+
+var settings = {};
 
 plugin.init = function(params, callback) {
 	var router = params.router,
@@ -12,20 +16,48 @@ plugin.init = function(params, callback) {
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
 
-	router.get('/admin/plugins/quickstart', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/quickstart', controllers.renderAdminPage);
+	router.get('/admin/plugins/domainverify', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
+	router.get('/api/admin/plugins/domainverify', controllers.renderAdminPage);
+
+	Meta.settings.get('domainverify', function(err, _settings) {
+		if (err) {
+			return winston.error(err);
+		}
+		settings = _settings;
+	});
 
 	callback();
 };
 
 plugin.addAdminNavigation = function(header, callback) {
 	header.plugins.push({
-		route: '/plugins/quickstart',
+		route: '/plugins/domainverify',
 		icon: 'fa-tint',
-		name: 'Quickstart'
+		name: 'Domain Verification'
 	});
 
 	callback(null, header);
+};
+
+plugin.verify = function(data, callback) {
+	var email = data.userData.email;
+	winston.info("Verifying email: " + email);
+
+	var domains = settings['allowed-domains'].split(",");
+	var matches = false;
+
+	domains.forEach(function (domain) {
+		winston.info("Checking for domain:" + domain);
+		if (email.endsWith("@" + domain)) {
+			matches = true;
+		}
+	});
+
+	if (matches) {
+		callback(null, data);
+	} else {
+		return callback(new Error("Invalid Email domain"));
+	}
 };
 
 module.exports = plugin;
